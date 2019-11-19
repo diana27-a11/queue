8c607f39a09b261a8e09a774b9378b348901511c
diana27-a11 <laura1027laura@gmail.com>
2019-11-19 13:05:25 -0600

    aaa

---
+++ src/queuelinkkedlist/QueueLinkKedList.java
@@ -0,0 +1,22 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queuelinkkedlist;
+
+/**
+ *
+ * @author Alumno
+ */
+public class QueueLinkKedList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
